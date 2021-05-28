a99976ace18210adb22563ab1c25246a8b88bfeb
Will Sumner <williamasumner@gmail.com>
2021-05-28 13:36:06 -0400

    Clean up main.ino

--- src/tlc5948-debug/main.ino
+++ src/tlc5948-debug/main.ino
@@ -1,17 +1,5 @@
-#include <MD_AD9833.h>
 #include "Tlc5948.h"
-#if DEBUG_TIMING
-int numTimes = 0;
-unsigned long totalTime = 0;
-#endif
-
-int const analogPin = A2;
-int const ledPin = 6; // pin d6 -> default freq is 980 Hz, timer1->millis()/micros()
-
-#define FSYNC  10
-//MD_AD9833  AD(FSYNC);  // Function Generator (using Hardware SPI)
 Tlc5948     tlc; // PWM LED driver (using Hardware SPI)
-
 
 inline void printChannels(Channels c) {
     Serial.println(static_cast<int>(c),HEX);
@@ -34,19 +22,13 @@ inline void printSidFlags(SidFlags s) {
 }
 
 void setup() {
-#if DEBUG_TIMING
-#warning "using debug timing"
-    Serial.begin(9600);
-    TCCR0B = TCCR0B & B11111000 | B00000001; // Adjusting PWM frequency of Timer1 to 61Khz - Not sure how this works? 
-#endif
-
     TCCR1B = TCCR1B & B11111000 | B00000001; // increase PWM frequency to 31Khz, 490Hz may be too slow
 
     Serial.begin(9600);
 
     pinMode(ledPin,OUTPUT);
     pinMode(analogPin,INPUT);
-    delay(3000);
+    delay(3000); // LEDs are somehow one before this completes
 
     SPI.begin();// TLC5948 Interface
     tlc.begin(); // sets up pins, default GS/DC/BC data and Func Ctrl bits
@@ -84,60 +66,5 @@ void setup() {
 
 }
 
-
-inline void funcToRun() {
-    //tlc.updateGsData(Channels::all,0x8000); // 0x8000 -> AA54
-    //tlc.updateGsData(Channels::all,0x00ff); // 0x00ff -> 0000 (all warnings except TEF)
-    //.tlc.updateGsData(Channels::all,0x0100); // 0x0100 -> 5522 (all warnings except TEF)
-    //tlc.updateGsData(Channels::all,0x0001); // 0x0001 -> 2a55 (all warnings except TEF)
-    tlc.updateGsData(Channels::all,0x0000); // 0x0000 -> XXXX (all warnings except TEF)
-    Serial.print("gs: \t");
-    tlc.printGsDataBuf();
-
-    Channels old = Channels::none, lsd = Channels::none, lod = Channels::none;
-    //digitalWrite(SSEL, LOW); // SSEL used for SPI with a slave select
-    tlc.exchangeData(DataKind::gsdata);
-    //digitalWrite(SSEL, HIGH);
-    // Pin 9 is PWM so ~490Hz -> 0.03469s or 34.69ms wait time required before retrieving Sid
-
-    SidFlags flags = tlc.getSidData(old,lsd,lod,true);
-    Serial.print("spi:\t");
-    tlc.printSpiBuf();
-
-    if (flags != SidFlags::BADPARSE) {
-        Serial.print("TLC5948: Received Error flags: ");
-        printSidFlags(flags);
-        Serial.print("lod channels: ");
-        printChannels(lod);
-        Serial.print("lsd channels: ");
-        printChannels(lsd);
-        Serial.print("old channels: ");
-        printChannels(old);
-    }
-    
-    delay(1000);
-
-}
-
-
 void loop() {
-#if DEBUG_TIMING
-    unsigned long timeStart = micros();
-#endif
-
-    //funcToRun();
-
-#if DEBUG_TIMING
-    unsigned long runTime = micros() - timeStart;
-    totalTime += runTime;
-
-    numTimes++;
-    if (numTimes >= 10000) {
-        Serial.print("Average time: ");
-        Serial.print(totalTime/10000.0);
-        Serial.print("us\n");
-        numTimes  =  0;
-        totalTime = 0;
-    }
-#endif
 }
