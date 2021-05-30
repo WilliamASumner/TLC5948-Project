a01c05b6bce7316164f75728ee04234b2e8ef4e5
Will Sumner <williamasumner@gmail.com>
2021-05-30 18:05:31 -0400

    Clean up comments and code

--- src/tlc5948-debug/main.ino
+++ src/tlc5948-debug/main.ino
@@ -2,7 +2,7 @@
 Tlc5948     tlc; // PWM LED driver (using Hardware SPI)
 
 inline void printChannels(Channels c) {
-    Serial.println(static_cast<int>(c),HEX);
+    Serial.println(static_cast<unsigned int>(c),HEX);
 }
 
 inline void printSidFlags(SidFlags s) {
@@ -22,35 +22,30 @@ inline void printSidFlags(SidFlags s) {
 }
 
 void setup() {
-    TCCR1B = TCCR1B & B11111000 | B00000001; // increase PWM frequency to 31Khz, 490Hz may be too slow
-
     Serial.begin(9600);
-    delay(3000); // LEDs are somehow one before this completes
+    delay(3000); // LEDs are somehow on before this completes
 
     SPI.begin();// TLC5948 Interface
     tlc.begin(); // sets up pins, default GS/DC/BC data and Func Ctrl bits
 
     tlc.updateGsData(Channels::all,0xFFFF); // all channels 100%
-
-    Serial.print("Greyscale data:\t");
-    tlc.printGsDataBuf();
-
     tlc.exchangeData(DataKind::gsdata);
-    delay(1000);
 
-    tlc.updateDcData(Channels::all,0x7F); // all channels high
-    Fctrls f = tlc.getFctrlBits()  & ~(Fctrls::blank_mask); // clear blank and timing reset bits
+    tlc.updateDcData(Channels::all,0x7F); // all channels DC high (maximum current defined by IREF ~ 20ma)
+    Fctrls f = tlc.getFctrlBits()  & ~(Fctrls::blank_mask); // clear blank bit
     tlc.updateFctrlData(f);
 
     Serial.print("Control data:\t");
     tlc.printCtrlDataBuf();
 
     tlc.exchangeData(DataKind::controldata);
+    Serial.println("Exchanged Control data");
     tlc.startGsclk();
+    Serial.println("started GSCLK");
 
     Serial.println("Getting SID data...");
     Channels old = Channels::none, lsd = Channels::none, lod = Channels::none;
-    SidFlags flags = tlc.getSidData(old,lsd,lod,true); // get Sid data
+    SidFlags flags = tlc.getSidData(old,lsd,lod,true);
 
     Serial.print("TLC5948: Received Error flags: ");
     printSidFlags(flags);
